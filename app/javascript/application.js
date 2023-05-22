da9a06d47871c5da8b6682570a584aa8d9431118
Mark Bussey <mark@curationexperts.com>
2023-05-22 14:31:37 -0500

    Dynamically reload Theme CSS
    Blacklight does not appear to provide a clean way to hook additional
    run-time stylesheet links into the core layout.
    
    This work-around adds a small JavaScript function that injects a new
    stylesheet link into the header after the layout is rendered.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,17 @@ import "controllers"
 import bootstrap from "bootstrap"
 import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
+
+function addThemeCss(fileName) {
+
+    var head = document.head;
+    var link = document.createElement("link");
+
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = fileName;
+
+    head.appendChild(link);
+}
+
+addThemeCss("/themes/current.css");
