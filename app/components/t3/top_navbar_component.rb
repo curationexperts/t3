7fb849dc6d668b373757eefb116e5a112ac3880a
Mark Bussey <mark@curationexperts.com>
2023-06-19 12:23:36 -0500

    Add preliminary color support for search bar
    This initial pass employs two different methods to inject dynamic
    CSS (and remove undesired Blacklight styling):
    * Redefine Blacklight view components directly
    * Define new view components and dynamically replace the provided
      Blacklight view components
    
    This provides us with a working proof-of-concept, but eventually
    we need a consistent pattern.
    
    View components are intended to allow us to write simple replacements,
    but so far it feels like we've had to bring in a large amount of code
    to make relatively minor styling changes.
    
    Ideally, we'd like to be able to make smaller interventions into
    Blacklight.  It's not clear whether the current state is just how
    view components work or whether it's a side effect of Blacklight
    incorporating too much styling into the components directly.
    
    TODO:
    * Choose one of the two patterns: override vs. inject new components
    * Figure out if there's way to achieve the styling results without
      needing to touch so many components

--- app/components/t3/top_navbar_component.rb
+++ app/components/t3/top_navbar_component.rb
@@ -1,27 +1,17 @@
 # frozen_string_literal: true
 
-module Blacklight
+module T3
   # Responsible for branding and menu items in the header.
   # Replaces native Blacklight version so we can use a image
   # instead of an image background for the brand logo.
-  class TopNavbarComponent < Blacklight::Component
-    def initialize(blacklight_config:)
-      super
-      @blacklight_config = blacklight_config
-    end
-
-    attr_reader :blacklight_config
-
-    delegate :application_name, :container_classes, to: :helpers
+  class TopNavbarComponent < Blacklight::TopNavbarComponent
+    include T3::Themeable
 
     def logo_link(_title: application_name)
       link_to logo_img, blacklight_config.logo_link, class: 'mb-0 navbar-brand navbar-logo'
     end
 
     def logo_img
-      requested_id = params[:theme_id]
-      theme = Theme.find_by(id: requested_id) || Theme.current
-
       if theme.main_logo.attached?
         image_tag(theme.main_logo)
       else
