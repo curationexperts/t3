9f8ab029db9f305eb311d98fc5eae0a50ba1b7a1
Mark Bussey <mark@curationexperts.com>
2024-04-14 13:11:43 -0500

    Add a custom form helper for displaying collection options
    **ISSUE**
    We want to be able to provide a collection drop-down selection list
    in our collection and item edit forms, but we want it to handle
    multivalue fields by displaying individual inputs that aggregate
    as an array valued parameter - i.e. each takes a single value and the
    field name ends in '[]' - instead of a multi-value select box.
    
    **SOLUTION**
    Implement a custom input that provides the desired behaviors for our
    forms.

---
+++ app/helpers/t3_form_builder.rb
@@ -0,0 +1,15 @@
+# Custom form controls to support T3 field-specific data types
+# e.g. #vacabulary provides a customized select populated with a defined vocabulary
+class T3FormBuilder < ActionView::Helpers::FormBuilder
+  def vocabulary(method, options = {})
+    multiple = options.delete(:multiple)
+    selected = options.delete(:value) || ''
+    select_options = options.except(:multiple)
+                            .reverse_merge(
+                              { name: @template.field_name(@object_name, method, multiple: multiple) }
+                            )
+                            .merge({ selected: selected })
+    option_tags = @template.options_from_collection_for_select(Collection.order(:created_at), :id, :id, selected)
+    select(method, option_tags, { prompt: 'Select one', selected: '', disabled: true }, select_options)
+  end
+end
