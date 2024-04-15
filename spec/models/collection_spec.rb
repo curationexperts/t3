776eab4d91733ae38fedc23c928e88166a64adb8
Mark Bussey <mark@curationexperts.com>
2024-04-15 14:40:30 -0500

    Refactor Item and Collection model tests to use shared examples
    The tests for the Item model and the Collection model had diverged
    as we more fully implemented Collections.  We also discovered cases
    where we had hard-coded Item class operations into the Resource class.
    
    This commit merges the Item and Collection tests and refactors them
    to be run as [RSpec shared examples](https://rspec.info/features/3-12/rspec-core/example-groups/shared-examples/).
    
    It also implements minor corrections to the Resource class to
    ensure class methods work with the desired subclasses.

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
+require './spec/models/resource_shared_examples'
 
 RSpec.describe Collection do
-  let(:collection) { described_class.new(blueprint: blueprint) }
-  let(:blueprint) { FactoryBot.create(:blueprint) }
-
-  it 'behaves like an Item' do
-    expect(collection).to be_a(Resource)
-  end
-
-  it 'inlcudes the model in its solrization' do
-    expect(collection.to_solr['model_ssi']).to eq 'Collection'
-  end
-
-  it 'saves to its own table', :aggregate_failures do
-    # Stub solr calls which are tested elsewhere
-    allow(collection).to receive(:update_index)
-
-    collection.metadata['Title'] = 'Sample'
-    expect { collection.save! }.not_to change(Item, :count)
-    expect { collection.save! }.not_to raise_exception
-  end
+  it_behaves_like 'a resource'
 end
