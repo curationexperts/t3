83d0e55b5ad8de6ed965274f31b0136a00d8431c
Mark Bussey <mark@curationexperts.com>
2024-04-25 17:08:34 -0500

    Improve Item and Collection display in dashboard
    This commit cleans up the UI for resources in the dashboard to
    make them easier to scan and parse.

--- spec/views/admin/collections/index.html.erb_spec.rb
+++ spec/views/admin/collections/index.html.erb_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 # The CollectionsController inherits from ItemsController, so uses
 # the items/views, but with a different controller name
 RSpec.describe 'admin/items/index' do
+  let(:collection_names) { ['Photos', 'Ephemera', 'Correspondence'] }
   let(:collections) do
-    FactoryBot.build_list(:collection, 3) { |coll, i| coll.id = i }
+    FactoryBot.build_list(:collection, 3) do |coll, i|
+      coll.id = i
+      coll.metadata['Title'] = collection_names[i]
+    end
   end
 
   before do
     allow(view).to receive(:controller_name).and_return('collections')
     collections.each { |coll| allow(coll).to receive(:persisted?).and_return(true) }
+    allow(Blueprint).to receive(:fields).and_return([FactoryBot.build(:field, name: 'Title')])
   end
 
   # An empty repository on initialization
@@ -23,7 +28,7 @@ RSpec.describe 'admin/items/index' do
       expect(rendered).to have_selector('h1', text: 'Collections')
     end
 
-    it 'has a link to create a collection' do
+    it 'has a link to create a new collection' do
       render
       expect(rendered).to have_link(href: new_collection_path, text: 'New collection')
     end
@@ -37,6 +42,17 @@ RSpec.describe 'admin/items/index' do
     it 'renders a list of collections' do
       render
       expect(rendered).to have_selector('div.resource', count: 3)
+    end
+
+    it 'displays labels' do
+      fragment = Capybara.string(render)
+      expect(fragment.all('.resource_label').map(&:text)).to eq collection_names
+    end
+
+    it 'displays the blueprint', :aggregate_failures do
+      render
+      expect(rendered).to have_selector('.blueprint_name', count: 3)
+      expect(rendered).to have_selector('.blueprint_name', text: collections[1].blueprint.name)
     end
 
     it 'provides a link to each collection', :aggregate_failures do
--- spec/views/admin/items/index.html.erb_spec.rb
+++ spec/views/admin/items/index.html.erb_spec.rb
@@ -1,26 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'admin/items/index' do
-  before do
-    assign(:items, [
-             Item.new(
-               blueprint: Blueprint.first,
-               metadata: {},
-               id: 'not-persisted-1'
-             ),
-             Item.new(
-               blueprint: Blueprint.first,
-               metadata: {},
-               id: 'not-persisted-2'
-             )
-           ])
+  let(:items) do
+    FactoryBot.build_list(:item, 3) { |item, i| item.id = i }
+  end
 
+  before do
     allow(view).to receive(:controller_name).and_return('items')
+    assign(:items, items)
   end
 
   it 'renders a list of items' do
     render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 6
+    expect(rendered).to have_selector('div.resource', count: 3)
   end
 end
