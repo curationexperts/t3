ace0789ac6d4d947dce30c3b8567a4fa8d79fab4
Mark Bussey <mark@curationexperts.com>
2024-04-25 16:42:13 -0500

    [REFACTOR] Handle resource labels for missing titles
    It "shouldn't" happen in the wild, but there are situations in
    development and testing where we may have a resource with an
    empty title field (i.e. first field defined in the blueprint).
    
    This refactoring ensures that #label always returns some identifiable
    label, either the "Title" defined by the blueprint, or the
    resource class and ID as a placeholder.
    
    We've also memoized the label's field name once a blueprint is loaded
    since we don't want to keep having to reload Fields each time we want
    to look up the label value.

--- app/models/blueprint.rb
+++ app/models/blueprint.rb
@@ -5,6 +5,14 @@ class Blueprint < ApplicationRecord
   validates :name, format: { with: /\A([\w _-])+\z/, message: 'can not contain special characters' }, allow_blank: true
 
   def fields
+    @fields ||= self.class.fields
+  end
+
+  def label_field
+    @label_field ||= fields.first&.name
+  end
+
+  def self.fields
     Field.active.order(:sequence)
   end
 
