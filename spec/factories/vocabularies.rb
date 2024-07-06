df76798f2264065f06a04d8b88cc38551b0933d1
Mark Bussey <mark@curationexperts.com>
2024-07-05 19:13:06 -0500

    Improve Vocabulary show and edit views
    This commit starts to refine some of the scaffold UI into something
    tuned for our application. This commit shows the desired direction
    that we'll need to apply to other views (new) and term views as well.

--- spec/factories/vocabularies.rb
+++ spec/factories/vocabularies.rb
@@ -2,6 +2,9 @@ FactoryBot.define do
   factory :vocabulary do
     label { Faker::Lorem.unique.words.join(' ').titleize }
 
+    # sets the key if it hasn't been provided
+    after(:build, &:validate)
+
     factory(:vocabulary_with_note) do
       note { Faker::Lorem.sentence }
     end
