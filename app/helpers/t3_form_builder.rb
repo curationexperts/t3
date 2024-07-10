f11072c7e1020643bca94dbf5c931d2c3df5a1de
Mark Bussey <mark@curationexperts.com>
2024-07-10 12:14:45 -0500

    Add form helper to render local vocabulary terms
    Previously, we had used the terminology 'voczbulary' for
    the list of collection names and had a corresponding FormBuilder
    helper.
    
    We want to separate code to support collecitons in the UI from
    code to support vocabularies. This change adds a separate helper
    that renders a selection list of the terms associated with a
    specific vocabulary.

--- app/helpers/t3_form_builder.rb
+++ app/helpers/t3_form_builder.rb
@@ -1,7 +1,7 @@
 # Custom form controls to support T3 field-specific data types
 # e.g. #vacabulary provides a customized select populated with a defined vocabulary
 class T3FormBuilder < ActionView::Helpers::FormBuilder
-  def vocabulary_field(method, options = {})
+  def collection_field(method, options = {})
     multiple = options.delete(:multiple)
     selected = options.delete(:value) || ''
     select_options = options.except(:multiple)
@@ -12,5 +12,21 @@ class T3FormBuilder < ActionView::Helpers::FormBuilder
     option_tags = @template.options_from_collection_for_select(Collection.order(:created_at), :label, :label, selected)
     select(method, option_tags, { prompt: 'Select one', selected: '', disabled: true }, select_options)
   end
-  alias collection_field vocabulary_field
+
+  # Render a slect box with options populated from a local vocabulary
+  # @param options [Hash] primary options include
+  #   :vocabualry - the vocabulary to display
+  #   :selected - the current value for the input (must match the key of one of the vocabulary terms)
+  #   :multiple - flag whether this form parameter should be named as an individual field or part of an array []
+  #   :prompt - defaults to 'Select one', accepts an alternate string, or pass `false` to suppress
+  #   see Rails FormBuilder#select for additional options
+  def vocabulary_field(method, options = {}, html_options = {})
+    vocabulary = options.delete(:vocabulary)
+    multiple = options.delete(:multiple)
+    html_options[:name] ||= @template.field_name(@object_name, method, multiple: multiple)
+    options[:prompt] ||= 'Select one'
+    choices = vocabulary.terms.order(:label, :key).map { |term| [term.label, term.key] }
+
+    select(method, choices, options, html_options)
+  end
 end
