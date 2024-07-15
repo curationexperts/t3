ae2740bde23ff07e3ee3a6fb21730e28664385d5
Mark Bussey <mark@curationexperts.com>
2024-07-15 10:49:55 -0500

    [REFACTOR] Split large shared test group into smaller groups
    **ISSUE**
    The Resource shared examples were becomming very long and we want to
    add tests for additional features.
    
    **RESOLUTION**
    Split the examples related to Solr indexing behaviors into their
    own file.

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 require './spec/models/resource_shared_examples'
+require './spec/models/indexed_resource_shared_examples'
 
 RSpec.describe Collection do
   it_behaves_like 'a resource'
+  it_behaves_like 'an indexed resource'
 end
